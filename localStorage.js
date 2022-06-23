526a31fda57327668f2eca46c34a03432e071333
Shuhad786 <shuhad.loofer@gmail.com>
2022-06-23 20:28:06 +0200

    update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,19 +1,20 @@
 const form = document.querySelector('.form-info');
 
-const {fname, lname, name, email, messagebox} = form.elements;
+const { fname, lname, name, email, messagebox } = form.elements;
 
 if (!localStorage.getItem('formObj')) {
   const formData = { fnameData: '', lnameData: '', nameData: '', emailData: '', messageData: '' };
 
-  localStorage.setItem('formObj', JSON.stringify(formData))
+  localStorage.setItem('formObj', JSON.stringify(formData));
 }
 
-const reservedata=(element, val) => {
-  element.addEventListener( 'change', (e) =>{
-  const data = element.value;
-  const formData = JSON.parse(localStorage.getItem('formObj'));
-  formData[val] = data;
-  localStorage.setItem('formObj', JSON.stringify(formData));})
+const reservedata = (element, val) => {
+    element.addEventListener('change', () => {
+    const data = element.value;
+    const formData = JSON.parse(localStorage.getItem('formObj'));
+    formData[val] = data;
+    localStorage.setItem('formObj', JSON.stringify(formData));
+});
 }
 
 reservedata(fname, 'fnameData')
