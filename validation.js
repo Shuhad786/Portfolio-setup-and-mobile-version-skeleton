e1ac9d704e9de0263a87ce3ec4286e9abe07b3a4
Shuhad786 <shuhad.loofer@gmail.com>
2022-06-23 20:46:39 +0200

    update js

--- validation.js
+++ validation.js
@@ -1,14 +1,12 @@
 const submitBtn = document.querySelector('.contact-button');
-const validateEmail = (email) => {
-  return email === email.toLowerCase();
-};
+const validateEmail = (email) => email === email.toLowerCase();
 
 submitBtn.addEventListener('click', () => {
   const emailVal = document.querySelector('.email').value;
 
   if (!validateEmail(emailVal)) {
     window.alert(
-      'Please enter email as lower case eg. shuhad.loofer@gmail.com'
+      'Please enter email as lower case eg. shuhad.loofer@gmail.com',
     );
-  }
+  };
 });
