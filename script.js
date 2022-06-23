7a92d219939863bfe15d1c2b95796eefd0e57829
Shuhad786 <shuhad.loofer@gmail.com>
2022-06-23 16:50:19 +0200

    Update js html

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const popupopen = document.querySelector('.button');
 const popupopen2 = document.querySelector('.button2');
 const popupopen3 = document.querySelector('.button3');
 const popupopen4 = document.querySelector('.button4');
-const popupwindow = document.querySelector('.popup-container');
-const popupclose = document.querySelector('.popup-close');
+
 const maincontainer = document.querySelector('.mobile-container');
 
 hamburger.addEventListener('click', () => {
@@ -18,35 +17,10 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-popupopen.addEventListener('click', () => {
-  popupwindow.classList.toggle('show');
-  maincontainer.style.filter = 'blur(50px)';
-});
-
-popupopen2.addEventListener('click', () => {
-  popupwindow.classList.toggle('show');
-  maincontainer.style.filter = 'blur(50px)';
-});
-
-popupopen3.addEventListener('click', () => {
-  popupwindow.classList.toggle('show');
-  maincontainer.style.filter = 'blur(50px)';
-});
-
-popupopen4.addEventListener('click', () => {
-  popupwindow.classList.toggle('show');
-  maincontainer.style.filter = 'blur(50px)';
-});
-
-popupclose.addEventListener('click', () => {
-  popupwindow.classList.remove('show');
-  maincontainer.style.filter = 'blur(0px)';
-});
-
 const PopupArray = [{
   title: 'Multi-Post Stories',
   img: './images/projectImg.png',
-  imgDiv: './images/Rectangle%2070.png',
+  imgDiv: './images/Rectangle70.png',
   text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
   techstack1: 'html',
   techstack2: 'Ruby on rails',
@@ -54,7 +28,7 @@ const PopupArray = [{
   techstack4: 'Github',
 }];
 
-const ContainerPopup = document.getElementsByClassName('popup');
+const ContainerPopup = document.querySelector('.popup');
 
 for (let i = 0; i < PopupArray.length; i += 1) {
   const card = document.createElement('div');
@@ -81,7 +55,7 @@ for (let i = 0; i < PopupArray.length; i += 1) {
 
   const image = document.createElement('img');
   image.classList.add('popup-project-image');
-  image.innerHTML = PopupArray[i].img;
+  image.src = PopupArray[i].img;
 
   const ContentText = document.createElement('p');
   ContentText.classList.add('popup-text');
@@ -89,31 +63,47 @@ for (let i = 0; i < PopupArray.length; i += 1) {
 
   const imageDivider = document.createElement('img');
   imageDivider.classList.add('popup-divider');
-  imageDivider.innerHTML = PopupArray[i].imgDiv;
+  imageDivider.src = PopupArray[i].imgDiv;
+
+  const imageDivider2 = document.createElement('img');
+  imageDivider2.classList.add('popup-divider');
+  imageDivider2.src = PopupArray[i].imgDiv;
+
+  const imageDivider3 = document.createElement('img');
+  imageDivider3.classList.add('popup-divider');
+  imageDivider3.src = PopupArray[i].imgDiv;
+
+  const imageDivider4 = document.createElement('img');
+  imageDivider4.classList.add('popup-divider');
+  imageDivider4.src = PopupArray[i].imgDiv;
+
+  const imageDivider5 = document.createElement('img');
+  imageDivider5.classList.add('popup-divider');
+  imageDivider5.src = PopupArray[i].imgDiv;
 
   const ul = document.createElement('ul');
   ul.classList.add('popup-tech-container');
 
   const li1 = document.createElement('li');
-  li1.innerHTML = imageDivider;
+  li1.appendChild(imageDivider);
 
   const li2 = document.createElement('li');
   li2.innerHTML = PopupArray[i].techstack1;
 
   const li3 = document.createElement('li');
-  li3.innerHTML = imageDivider;
+  li3.appendChild(imageDivider2);
 
   const li4 = document.createElement('li');
   li4.innerHTML = PopupArray[i].techstack2;
 
   const li5 = document.createElement('li');
-  li5.innerHTML = imageDivider;
+  li5.appendChild(imageDivider3);
 
   const li6 = document.createElement('li');
   li6.innerHTML = PopupArray[i].techstack3;
 
   const li7 = document.createElement('li');
-  li7.innerHTML = imageDivider;
+  li7.appendChild(imageDivider4);
 
   const li8 = document.createElement('li');
   li8.classList.add('Git');
@@ -121,7 +111,7 @@ for (let i = 0; i < PopupArray.length; i += 1) {
 
   const li9 = document.createElement('li');
   li9.classList.add('Git');
-  li9.innerHTML = imageDivider;
+  li9.appendChild(imageDivider5);
 
   const btn1 = document.createElement('button');
   btn1.classList.add('btn1');
@@ -151,3 +141,30 @@ for (let i = 0; i < PopupArray.length; i += 1) {
   ContainerPopup.appendChild(card).appendChild(btn2);
   ContainerPopup.appendChild(card).appendChild(btnRow);
 }
+const popupwindow = document.querySelector('.popup-container');
+const popupclose = document.querySelector('.popup-close');
+
+popupopen.addEventListener('click', () => {
+  popupwindow.classList.toggle('show'); // here
+  maincontainer.style.filter = 'blur(50px)';
+});
+
+popupopen2.addEventListener('click', () => {
+  popupwindow.classList.toggle('show');
+  maincontainer.style.filter = 'blur(50px)';
+});
+
+popupopen3.addEventListener('click', () => {
+  popupwindow.classList.toggle('show');
+  maincontainer.style.filter = 'blur(50px)';
+});
+
+popupopen4.addEventListener('click', () => {
+  popupwindow.classList.toggle('show');
+  maincontainer.style.filter = 'blur(50px)';
+});
+
+popupclose.addEventListener('click', () => { // here
+  popupwindow.classList.remove('show');
+  maincontainer.style.filter = 'blur(0px)';
+});
